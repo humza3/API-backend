0f4d352f13f87a86ea08f9ba5c7127ccfa9d4816
humza3 <humzabutt3@gmail.com>
2020-05-31 08:22:06 +0100

    added save and retrieve logic to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Sauce = require('./models/sauce');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://humza:fyumnEy1J0pNrkR0@cluster0-mzxo9.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true,useNewUrlParser: true })
@@ -23,11 +25,66 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Thing created successfully!'
+
+
+app.post('/api/sauces', (req, res, next) => {
+  const sauce = new Sauce({
+	_id: req.body._id,
+	userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    userLiked: req.body.userLiked,
+    userDisliked: req.body.userDisliked
   });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.use('/api/sauces', (req, res, next) => {
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 
 app.use((req, res, next) => {
