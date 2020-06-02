0aa37155fa32b340045a38140f4033a8e2384c05
humza3 <humzabutt3@gmail.com>
2020-06-02 15:26:11 +0100

    setup the controllers folder moved the code form routes folder to separate controller file

---
+++ controllers/stuff.js
@@ -0,0 +1,112 @@
+const Sauce = require('../models/sauce');
+
+//save sauces to the database
+exports.createSauce = (req, res, next) => {
+  const sauce = new Sauce({
+	_id: req.body.id,
+	userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    userLiked: req.body.userLiked,
+    userDisliked: req.body.userDisliked
+  });
+  sauce.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//display a single sauce on  a page
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+//update sauces with modifications
+exports.modifySauce = (req, res, next) => {
+  const sauce = new Sauce({
+    _id: req.params.id,
+	userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    userLiked: req.body.userLiked,
+    userDisliked: req.body.userDisliked
+  });
+  Sauce.updateOne({_id: req.params.id}, sauce).then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//deletesauce from database and page
+exports.deleteSauce = (req, res, next) => {
+  Sauce.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+//retreive and list sauces for sale
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+}
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,117 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const Sauce = require('../models/sauce')
+const stuffCtrl = require('../controllers/stuff');
 
-//save sauces to the database
-router.post('/', (req, res, next) => {
-  const sauce = new Sauce({
-	_id: req.body.id,
-	userId: req.body.userId,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    mainPepper: req.body.mainPepper,
-    imageUrl: req.body.imageUrl,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    userLiked: req.body.userLiked,
-    userDisliked: req.body.userDisliked
-  });
-  sauce.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-//display a single sauce onn  a page
-router.get('/:id', (req, res, next) => {
-  Sauce.findOne({
-    _id: req.params.id
-  }).then(
-    (sauce) => {
-      res.status(200).json(sauce);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
-});
-
-//update suaces with modifications
-router.put('/:id', (req, res, next) => {
-  const sauce = new Sauce({
-    _id: req.params.id,
-	userId: req.body.userId,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    mainPepper: req.body.mainPepper,
-    imageUrl: req.body.imageUrl,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    userLiked: req.body.userLiked,
-    userDisliked: req.body.userDisliked
-  });
-  Sauce.updateOne({_id: req.params.id}, sauce).then(
-    () => {
-      res.status(201).json({
-        message: 'Sauce updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-//deletesauce from datatbase and page
-router.delete('/:id', (req, res, next) => {
-  Sauce.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
 
 //retreive and list sauces for sale
-router.get('/', (req, res, next) => {
-  Sauce.find().then(
-    (sauces) => {
-      res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+router.get('/', stuffCtrl.getAllSauce);
+//save sauces to the database
+router.post('/', stuffCtrl.createSauce);
+//display a single sauce on  a page
+router.get('/:id', stuffCtrl.getOneSauce);
+//update suaces with modifications
+router.put('/:id', stuffCtrl.modifySauce);
+//deletesauce from database and page
+router.delete('/:id', stuffCtrl.deleteSauce);
+
 
 module.exports = router;
