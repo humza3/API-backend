06971d63d6b8568252cfe6603ae89fce6aa8433b
humza3 <humzabutt3@gmail.com>
2020-06-05 14:23:42 +0100

    implemented authentication middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
 
 //retreive and list sauces for sale
-router.get('/', stuffCtrl.getAllSauce);
+router.get('/', auth, stuffCtrl.getAllSauce);
 //save sauces to the database
-router.post('/', stuffCtrl.createSauce);
+router.post('/', auth, stuffCtrl.createSauce);
 //display a single sauce on  a page
-router.get('/:id', stuffCtrl.getOneSauce);
+router.get('/:id', auth, stuffCtrl.getOneSauce);
 //update suaces with modifications
-router.put('/:id', stuffCtrl.modifySauce);
+router.put('/:id', auth, stuffCtrl.modifySauce);
 //deletesauce from database and page
-router.delete('/:id', stuffCtrl.deleteSauce);
+router.delete('/:id', auth, stuffCtrl.deleteSauce);
 
 
 module.exports = router;
