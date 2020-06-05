f9c6f2fe0633372936dd94b950e5d5543cb4176d
humza3 <humzabutt3@gmail.com>
2020-06-05 14:12:54 +0100

    created authentication tokens

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
 
 exports.signup = (req, res, next) => {
 	bcrypt.hash(req.body.password, 10).then(
@@ -40,9 +42,13 @@ exports.login = (req, res, next) => {
 							error: new Error('Incorrect password!')
 						});
 					}
+					const token = jwt.sign(
+						{userId: user._id},
+						'RANDOM_TOKEN_SECRET',
+						{expiresIn: '24h'});
 					res.status(200).json({
 						userId: user._id,
-						token: 'token'
+						token: token
 					});
 				}
 			).catch(
